f607d5adab206920a9ffb1df5ddb59c1d038ce10
Gabriel Gustavo <gabriel.gustavo.s@hotmail.com>
2020-05-02 22:54:17 -0300

    updating

--- main.py
+++ main.py
@@ -6,14 +6,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    historicos_pagamentos = []
+    lista_pagamentos = []
     pagamentos = api_pagarme.busca_todas_transacao().json()
     
-    for pagamento in pagamentos:
-        historico_transacao = api_pagarme.busca_id_transacao(pagamento['transaction_id']).json()
-        historicos_pagamentos.append(historico_transacao)
+    for pagamento in pagamentos: 
+        transicao = {}
+        dados_transacao = api_pagarme.busca_id_transacao(pagamento['transaction_id']).json()
+        transicao['id'] = dados_transacao['tid']
+        
+        if  len(str(dados_transacao['amount'])) > 3:
+            transicao['preco'] = str(dados_transacao['amount'])[:-2]+",00"
+        else:
+            transicao['preco'] = str(dados_transacao['amount'])+",00"
+
+        transicao['name'] = dados_transacao['customer']['name']
+        transicao['email'] = dados_transacao['customer']['email']
+        
+        if dados_transacao['payment_method'] == 'credit_card':
+            transicao['pagamento'] = 'Cartão de crédito'
+        else:
+            transicao['pagamento'] = 'Boleto'
+
+        lista_pagamentos.append(transicao)
     
-    return render_template('index.html', lista_pagamentos = historicos_pagamentos)
+    return render_template('index.html', lista_pagamentos = lista_pagamentos)
 
 
 
--- services/pagarme.py
+++ services/pagarme.py
@@ -1,73 +1,6 @@
 import requests
 
-chave_api = "ak_test_PwBsJZ7NAfJCXvwNFKkPjLw7H1Wrps"
-
-params = {
-    "api_key": chave_api,
-		"amount": "25100",
-    "card_number": "4111111111111111",
-    "card_cvv": "123",
-    "card_expiration_date": "0922",
-    "card_holder_name": "Morpheus Fishburne",
-    "customer": {
-      "external_id": "1",
-      "name": "Morpheus Fishburne",
-      "type": "individual",
-      "country": "br",
-      "email": "mopheus@nabucodonozor.com",
-      "documents": [
-        {
-          "type": "cpf",
-          "number": "30621143049"
-        }
-      ],
-      "phone_numbers": ["+5511999998888", "+5511888889999"],
-      "birthday": "1965-01-01"
-    },
-    "billing": {
-      "name": "Trinity Moss",
-      "address": {
-        "country": "br",
-        "state": "sp",
-        "city": "Cotia",
-        "neighborhood": "Rio Cotia",
-        "street": "Rua Matrix",
-        "street_number": "9999",
-        "zipcode": "06714360"
-      }
-    },
-    "shipping": {
-      "name": "Neo Reeves",
-      "fee": "1000",
-      "delivery_date": "2000-12-21",
-      "expedited": True,
-      "address": {
-        "country": "br",
-        "state": "sp",
-        "city": "Cotia",
-        "neighborhood": "Rio Cotia",
-        "street": "Rua Matrix",
-        "street_number": "9999",
-        "zipcode": "06714360"
-      }
-    },
-    "items": [
-      {
-        "id": "r123",
-        "title": "Red pill",
-        "unit_price": "10000",
-        "quantity": "1",
-        "tangible": True
-      },
-      {
-        "id": "b123",
-        "title": "Blue pill",
-        "unit_price": "10000",
-        "quantity": "1",
-        "tangible": True
-      }
-    ]
-}
+chave_api = "SUA CHAVE API PAGAR.ME"
 
 def realizar_transicao(dados):
   dados['api_key'] = chave_api
@@ -87,8 +20,3 @@ def busca_id_transacao(id):
 def saldo(dados):
   r = requests.get('https://api.pagar.me/1/balance', json = {"api_key": chave_api})
   return r
-
-# print(realizar_transicao({"params": 1}))
-# print(busca_todas_transacao().json())
-# a = busca_todas_transacao().json()
-# print(saldo(params).json())
