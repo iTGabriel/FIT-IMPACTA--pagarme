2cd430a39c0ba65c302d3bdf9ec81fbfb6e4bea0
itGabriel <gabriel.gustavo.s@hotmail.com>
2020-05-06 00:43:30 -0300

    updating

--- services/pagarme.py
+++ services/pagarme.py
@@ -17,10 +17,6 @@ def busca_id_transacao(id):
   r = requests.get('https://api.pagar.me/1/transactions/'+str(id), json = {"api_key": chave_api})
   return [r, r.status_code]
 
-def saldo(dados):
-  r = requests.get('https://api.pagar.me/1/balance', json = {"api_key": chave_api})
-  return [r, r.status_code]
-
 def realizar_estorno(dados):
   dados['api_key'] = chave_api
   r = requests.post('https://api.pagar.me/1/transactions/'+dados['transaction_id']+'/refund', json=dados)
